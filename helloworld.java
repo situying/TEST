22886dcb32702355c6c7d3acdff93003ab975019
ysitu <ysitu@hengtiansoft.com>
2013-09-09 13:35:44 +0800

    provide public method for testString

--- helloworld.java
+++ helloworld.java
@@ -7,4 +7,14 @@ public class Test{
 	public static void main(String[] args){
 		System.out.println("Hello World！");
 	}
+	
+	public String getTestString()
+	{
+		return testString;
+	}
+	
+	public void setTestString(String testString)
+	{
+		this.testString = testString;
+	}
 }
